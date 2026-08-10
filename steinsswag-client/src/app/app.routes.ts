07935931880dd8ae861d35bd624ba990490ba8dd
BellahrachBilal <bilalbellahrech@gmail.com>
2026-08-10 14:49:18 +0200

    add routing

--- steinsswag-client/src/app/app.routes.ts
+++ steinsswag-client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { SellerListComponent } from './components/seller-list/seller-list.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { PlacementSlotListComponent } from './components/placement-slot-list/placement-slot-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'items', pathMatch: 'full' },
+  { path: 'items', component: ItemListComponent },
+  { path: 'sellers', component: SellerListComponent },
+  { path: 'categories', component: CategoryListComponent },
+  { path: 'placement-slots', component: PlacementSlotListComponent },
+];
--- steinsswag-client/src/app/app.ts
+++ steinsswag-client/src/app/app.ts
@@ -1,14 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ItemListComponent } from './components/item-list/item-list.component';
-import { SellerListComponent } from './components/seller-list/seller-list.component';
-import { PlacementSlotListComponent } from './components/placement-slot-list/placement-slot-list.component';
-import { CategoryListComponent } from './components/category-list/category-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , ItemListComponent, SellerListComponent,
-     PlacementSlotListComponent, CategoryListComponent],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
